285d459151b703c81b569f96396e3925efa044b2
@nair <nilah.nair@tu-dortmun.de>
2021-03-10 20:15:57 +0100

    Create type1_normalisation_values_imu.py

---
+++ type1_normalisation_values_imu.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 17:26:32 2021
+
+@author: nilah
+"""
+import os
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+import csv_reader
+
+
+headers_annotated = ['Time', 'Class', 'AccX_L', 'AccY_L', 'AccZ_L', 'GyrX_L', 'GyrY_L', 'GyrZ_L',
+           'MagX_L', 'MagY_L', 'MagZ_L', 'AccX_T', 'AccY_T', 'AccZ_T', 'GyrX_T', 'GyrY_T',
+           'GyrZ_T', 'MagX_T', 'MagY_T', 'MagZ_T', 'AccX_R', 'AccY_R', 'AccZ_R', 'GyrX_R',
+           'GyrY_R', 'GyrZ_R', 'MagX_R', 'MagY_R', 'MagZ_R']
+
+headers = ['Time', 'AccX_L', 'AccY_L', 'AccZ_L', 'GyrX_L', 'GyrY_L', 'GyrZ_L',
+           'MagX_L', 'MagY_L', 'MagZ_L', 'AccX_T', 'AccY_T', 'AccZ_T', 'GyrX_T', 'GyrY_T',
+           'GyrZ_T', 'MagX_T', 'MagY_T', 'MagZ_T', 'AccX_R', 'AccY_R', 'AccZ_R', 'GyrX_R',
+           'GyrY_R', 'GyrZ_R', 'MagX_R', 'MagY_R', 'MagZ_R']
+
+SCENARIO = {'R01': 'L01', 'R02': 'L01', 'R03': 'L02', 'R04': 'L02', 'R05': 'L02', 'R06': 'L02', 'R07': 'L02',
+            'R08': 'L02', 'R09': 'L02', 'R10': 'L02', 'R11': 'L02', 'R12': 'L02', 'R13': 'L02', 'R14': 'L02',
+            'R15': 'L02', 'R16': 'L02', 'R17': 'L03', 'R18': 'L03', 'R19': 'L03', 'R20': 'L03', 'R21': 'L03',
+            'R22': 'L03', 'R23': 'L03', 'R24': 'L03', 'R25': 'L03', 'R26': 'L03', 'R27': 'L03', 'R28': 'L03',
+            'R29': 'L03', 'R30': 'L03'}
+
+labels_persons = {"S01": 0, "S02": 1, "S03": 2, "S04": 3, "S05": 4, "S06": 5, "S07": 6, "S08": 7, "S09": 8,
+                  "S10": 9, "S11": 10, "S12": 11, "S13": 12, "S14": 13, "S15": 14, "S16": 15}
+
+def read_extracted_data(path, skiprows = 1):
+    '''
+    gets data from csv file
+    data contains 3 columns, start, end and label
+
+    returns a numpy array
+
+    @param path: path to file
+    '''
+
+    annotation_original = np.loadtxt(path, delimiter=',', skiprows=skiprows)
+    return annotation_original
+
+def statistics_measurements():
+    '''
+    Computes some statistics over the channels for the entire training data
+
+    returns a max_values, min_values, mean_values, std_values
+    '''
+
+    #dataset_path_imu = "/vol/actrec/DFG_Project/2019/LARa_dataset/Motionminers/2019/flw_recordings_12000/"
+    dataset_path_imu = "/vol/actrec/DFG_Project/2019/LARa_dataset/Mbientlab/LARa_dataset_mbientlab"
+
+    #train_final_ids = ["S07", "S08", "S09", "S10", "S11", "S12"]
+
+    persons = ["S07", "S08", "S09", "S10", "S11", "S12", "S13", "S14"]
+    recordings = ['R{:02d}'.format(r) for r in range(1, 31)]
+    
+    train_ids = ["R03", "R07", "R08", "R10"]
+    #val_ids = ["R12"]
+    #test_ids = ["R15"]
+
+    accumulator_measurements = np.empty((0, 27))
+    for P in persons:
+          for R in train_ids:
+                S = SCENARIO[R]
+                file_name_data = "{}/{}_{}_{}.csv".format(P, S, P, R)
+                #file_name_label = "{}/{}_{}_{}_labels.csv".format(P, S, P, R)
+                print("------------------------------\n{}".format(file_name_data))
+                try:
+                    # getting data
+                    data = read_extracted_data(dataset_path_imu + file_name_data, skiprows=1)
+                    data_x = data[:, 2:]
+                    accumulator_measurements = np.append(accumulator_measurements, data_x, axis=0)
+                    print("\nFiles loaded")
+                except:
+                    print("\n1 In loading data,  in file {}".format(dataset_path_imu + file_name_data))
+                    continue
+
+    try:
+        max_values = np.max(accumulator_measurements, axis=0)
+        min_values = np.min(accumulator_measurements, axis=0)
+        mean_values = np.mean(accumulator_measurements, axis=0)
+        std_values = np.std(accumulator_measurements, axis=0)
+    except:
+        max_values = 0
+        min_values = 0
+        mean_values = 0
+        std_values = 0
+        print("Error computing statistics")
+    return max_values, min_values, mean_values, std_values
+
+if __name__ == '__main__':
+    
+    #Computing Statistics of data
+    max_values, min_values, mean_values, std_values = statistics_measurements()
+    x = [max_values, min_values, mean_values, std_values]
+    
+    base_directory='/data/nnair/trial/'
+    
+    csv_dir=  base_directory+"type1_normalisation_values_imu.csv"
+    print(csv_dir)
+    np.savetxt(csv_dir, x, delimiter="\n", fmt='%s')
