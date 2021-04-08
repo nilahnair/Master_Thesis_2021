bc2605c728ae5db5ba029aaa52e2b5c2bed15041
@nair <nilah.nair@tu-dortmun.de>
2021-04-08 12:46:03 +0200

    Update main_id.py

--- main_id.py
+++ main_id.py
@@ -406,8 +406,11 @@ def my_config():
 @ex.capture
 def run(config, dataset, network, output, usageModus):
     print("run function began")
-    setup_experiment_logger(logging_level=logging.DEBUG,
-                            filename=config['/data/nnair/output/softmax/'] + "logger.txt")
+    filename=config['/data/nnair/output/softmax/']
+    print(filename)
+    filename=config['/data/nnair/output/softmax/']+"logger.txt"
+    print(filename)
+    setup_experiment_logger(logging_level=logging.DEBUG,filename="logger.txt")
 
     logging.info('Finished')
     logging.info('Dataset {} Network {} Output {} Modus {}'.format(dataset, network, output, usageModus))
