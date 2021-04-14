2f16f97a1263d948361d2c2d42c962d888ba772b
@nair <nilah.nair@tu-dortmun.de>
2021-04-14 16:48:38 +0200

    Update data_preprocessing_MoCap_norm.py

--- data_preprocessing_MoCap_norm.py
+++ data_preprocessing_MoCap_norm.py
@@ -140,7 +140,7 @@ NORM_MIN_THRESHOLDS = [ -356.7484,  -350.385,   -275.538,   -426.97,    -459.003
                        -405.712,   -457.251,   -565.544,   -796.613,   -767.2,     -692.469,
                        -405.712,   -457.251,  -565.544,   -887.707,   -815.67,   -712.609,
                        -260.2279,   -98.0932,   -66.7167,  -122.788,   -107.3,     -162.189]
-
+'''
 #type2 - avoiding subject 11
 
 NORM_MAX_THRESHOLDS = [ 385.977,   315.51,    284.478,   460.544,   455.554,   460.81,    385.977,
@@ -226,7 +226,7 @@ NORM_MIN_THRESHOLDS = [ -356.7484,   -350.385,    -287.806,   -451.56,     -459.
                        -405.712,    -407.182,    -565.544,    -796.613,    -852.581,    -695.073,
                        -405.712,    -407.182,    -565.544,    -887.707,    -947.151,    -753.931,
                        -78.9523,    -98.0932,   -164.68537,  -131.22,     -117.78,     -162.387]
-
+'''
 
 #type4 - avoiding subjects 10,11 and 12
 '''
@@ -348,7 +348,7 @@ def generate_data(ids, sliding_window_length, sliding_window_step, data_dir=None
     train_ids = ["R03", "R07", "R08", "R10", "R11"]
     val_ids = ["R12"]
     test_ids = ["R15"]
-   
+    '''
     #type2- avoiding person 11
     
     persons = ["S07", "S08", "S09", "S10", "S12", "S13", "S14"]
@@ -365,7 +365,7 @@ def generate_data(ids, sliding_window_length, sliding_window_step, data_dir=None
     train_ids = ["R03", "R07", "R08", "R10", "R11", "R12", "R15", "R18"]
     val_ids = ["R19", "R21"]
     test_ids = ["R22", "R23"]
-    
+    '''
     
     #type4-Avoiding persons 11,12,10
     '''
@@ -536,7 +536,7 @@ def create_dataset():
     train_ids = ["R03", "R07", "R08", "R10", "R11"]
     val_ids = ["R12"]
     test_ids = ["R15"]
-    
+    '''
     #type2- avoiding person 11
     
     train_ids = ["R11", "R12", "R15", "R18", "R19", "R21"]
@@ -549,7 +549,7 @@ def create_dataset():
     train_ids = ["R03", "R07", "R08", "R10" "R11", "R12", "R15", "R18"]
     val_ids = ["R19", "R21"]
     test_ids = ["R22", "R23"]
-    
+    '''
     
     #type4-Avoiding persons 11,12,10
     '''
@@ -558,7 +558,7 @@ def create_dataset():
     test_ids = ["R27", "R28", "R29"]
     '''
     
-    base_directory = '/data/nnair/output/type3/mocap/clean/'
+    base_directory = '/data/nnair/output/type2/mocap/clean/'
     sliding_window_length = 100
     sliding_window_step = 12
     
