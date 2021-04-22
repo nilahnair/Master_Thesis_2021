74d0097829887faeb8134d728ec4bf3b3036f39d
@nair <nilah.nair@tu-dortmun.de>
2021-04-22 11:11:34 +0200

    Update main_id.py

--- main_id.py
+++ main_id.py
@@ -21,7 +21,7 @@ from sacred import Experiment
 #from sacred.utils import apply_backspaces_and_linefeeds
 from sacred.observers import MongoObserver
 
-ex= Experiment('Type1 experiment imu norm lr2 batch200')
+ex= Experiment('Type2 experiment imu norm lr0 batch100')
 
 ex.observers.append(MongoObserver.create(url='curtiz',
                                          db_name='nnair_sacred',
@@ -146,11 +146,11 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
     batch_size_train = {
         'cnn': {'mocap': 100, 'mbientlab': 100, 'motionminers_flw': 100},
         'lstm': {'mocap': 100, 'mbientlab': 100, 'motionminers_flw': 100},
-        'cnn_imu': {'mocap': 100, 'mbientlab':200, 'motionminers_flw': 100}}
+        'cnn_imu': {'mocap': 100, 'mbientlab':100, 'motionminers_flw': 100}}
 
     batch_size_val = {'cnn': {'mocap': 100, 'mbientlab': 100, 'motionminers_flw': 100},
                       'lstm': {'mocap': 100, 'mbientlab': 100, 'motionminers_flw': 100},
-                      'cnn_imu': {'mocap': 100, 'mbientlab':200, 'motionminers_flw': 100}}
+                      'cnn_imu': {'mocap': 100, 'mbientlab':100, 'motionminers_flw': 100}}
     
      # Number of iterations for accumulating the gradients
     accumulation_steps = {'mocap': 4, 'mbientlab': 4, 'motionminers_flw': 4}
@@ -178,7 +178,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
 
     if output[output_idx] == 'softmax':
         labeltype = "class"
-        folder_base = "/data/nnair/output/softmax/"
+        folder_base = "/data/nnair/output/softmax2/"
     elif output[output_idx] == 'attribute':
         labeltype = "attributes"
         folder_base = "/data/nnair/output/attributes/"
@@ -192,7 +192,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
     if usage_modus[usage_modus_idx] == 'train':
         folder_exp = folder_base + dataset[dataset_idx] + '/' + \
                      network[network_idx] + '/' + fully_convolutional \
-                     + '/' + reshape_folder +'/' + 'experiment9/'
+                     + '/' + reshape_folder +'/' + 'experiment/'
         print(folder_exp)
         '''
         folder_exp_base_fine_tuning = folder_base + dataset[dataset_fine_tuning_idx] + '/' + \
@@ -281,17 +281,17 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
     path_to_datasets_folder='/data/nnair/output/type4/mocap/'
     '''
     #type1
-    
+    '''
     dataset_root = {'mocap': '/data/nnair/output/type1/mocap/unclean/',
                     'mbientlab': '/data/nnair/output/type1/imu_norm/unclean/',
                     'motionminers_flw': '/data/nnair/output/type1/momin/'}
-    
-    #type2
     '''
+    #type2
+    
     dataset_root = {'mocap': '/data/nnair/output/type2/mocap/unclean/',
                     'mbientlab': '/data/nnair/output/type2/imu_norm/unclean/',
                     'motionminers_flw': '/data/nnair/output/type2/momin/'}
-    '''
+    
     #type3
     '''
     dataset_root = {'mocap': '/data/nnair/output/type3/mocap/unclean/',
@@ -405,7 +405,7 @@ def my_config():
                            usage_modus_idx=0,
                            #dataset_fine_tuning_idx=0,
                            reshape_input=False,
-                           learning_rates_idx=2,
+                           learning_rates_idx=0,
                            name_counter=0,
                            freeze=0,
                            fully_convolutional=False,
@@ -425,9 +425,9 @@ def my_config():
 @ex.capture
 def run(config, dataset, network, output, usageModus):
    
-    file_name='/data/nnair/output/softmax/'
+    file_name='/data/nnair/output/softmax2/'
    
-    file_name='/data/nnair/output/softmax/'+'logger.txt'
+    file_name='/data/nnair/output/softmax2/'+'logger.txt'
     
     setup_experiment_logger(logging_level=logging.DEBUG,filename=file_name)
 
