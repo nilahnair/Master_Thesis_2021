bb7167fd61ba81c52a6f87372ffde7d885421f8e
@nair <nilah.nair@tu-dortmun.de>
2021-05-10 07:22:41 +0200

    metrics

--- network_user_act.py
+++ network_user_act.py
@@ -32,8 +32,9 @@ from network_act import Network
 
 from HARWindows_act import HARWindows
 
+#from metrics import Metrics
 from metrics_act import Metrics
-from metrics import Metrics
+
 
 
 class Network_User(object):
