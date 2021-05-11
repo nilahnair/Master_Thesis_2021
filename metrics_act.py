2ef2cc976d610d911ec4a932cdea030a87ac1469
@nair <nilah.nair@tu-dortmun.de>
2021-05-11 09:00:49 +0200

    Update metrics_act.py

--- metrics_act.py
+++ metrics_act.py
@@ -62,6 +62,8 @@ class Metrics(object):
         elif self.config['output'] == 'attribute':
             precision = torch.zeros((self.center.shape[0]))
             recall = torch.zeros((self.center.shape[0]))
+        
+        print(precision)
           
         x = torch.ones(predictions.size())
         y = torch.zeros(predictions.size())
@@ -94,6 +96,7 @@ class Metrics(object):
         elif self.config['output'] == 'attribute':
             for c in range(self.center.shape[0]):
                 selected_elements = torch.where(predictions == c, x, y)
+                print(selected_elements)
                 non_selected_elements = torch.where(predictions == c, y, x)
 
                 target_elements = torch.where(targets == c, x, y)
@@ -113,6 +116,8 @@ class Metrics(object):
                     #                                                                                                                              false_positives.item(),
                     #                                                                                                                              false_negatives.item()))
                     continue
+        print(precision)
+        print(recall)
        
         return precision, recall
 
@@ -201,13 +206,18 @@ class Metrics(object):
                     elif targets[i,0]==7:
                         targets[i,0]=6
             precision, recall = self.get_precision_recall(targets[:, 0], predictions)
-            
+        print(targets)
+        print(precision)
+        print(recall)
        
         if self.config['output'] == 'softmax':
             proportions = torch.zeros(self.config['num_classes'])
         elif self.config['output'] == 'attribute':
             proportions = torch.zeros(self.center.shape[0])
-
+        
+        print("proportions")
+        print(proportions)
+        
         if self.config['output'] == 'softmax':
             for c in range(self.config['num_classes']):
                 proportions[c] = torch.sum(targets == c).item() / float(targets.size()[0])
@@ -216,6 +226,7 @@ class Metrics(object):
             for c in range(self.center.shape[0]):
                 proportions[c] = torch.sum(targets[:, 0] == c).item() / float(targets[:, 0].size()[0])
         
+        print(proportions)
         logging.info('            Metric:    \nPrecision: \n{}\nRecall\n{}'.format(precision, recall))
 
         self.results['precision'] = precision
@@ -232,6 +243,8 @@ class Metrics(object):
         weighted_f1[np.isnan(weighted_f1)] = 0
 
         F1_weighted = torch.sum(weighted_f1) * 2
+        print(' F1_weighted ')
+        print( F1_weighted )
 
         # F1 mean
         f1 = multi_pre_rec / sum_pre_rec
