2daad9b73d481ad4d4d58f800b4607afac3b92a3
@nair <nilah.nair@tu-dortmun.de>
2021-05-13 21:36:05 +0200

    check

--- main_id_act.py
+++ main_id_act.py
@@ -120,11 +120,11 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
     batch_size_train = {
         'cnn': {'mocap': 100, 'mbientlab': 100, 'motionminers_flw': 100},
         'lstm': {'mocap': 100, 'mbientlab': 100, 'motionminers_flw': 100},
-        'cnn_imu': {'mocap': 100, 'mbientlab':100, 'motionminers_flw': 100}}
+        'cnn_imu': {'mocap': 50, 'mbientlab':100, 'motionminers_flw': 100}}
 
     batch_size_val = {'cnn': {'mocap': 100, 'mbientlab': 100, 'motionminers_flw': 100},
                       'lstm': {'mocap': 100, 'mbientlab': 100, 'motionminers_flw': 100},
-                      'cnn_imu': {'mocap': 100, 'mbientlab':100, 'motionminers_flw': 100}}
+                      'cnn_imu': {'mocap': 50, 'mbientlab':100, 'motionminers_flw': 100}}
     
      # Number of iterations for accumulating the gradients
     accumulation_steps = {'mocap': 4, 'mbientlab': 4, 'motionminers_flw': 4}
