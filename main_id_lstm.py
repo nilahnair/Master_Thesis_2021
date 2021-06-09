ff3c59f2bb1c6427af8d1d6aafa227fbb555e811
@nair <nilah.nair@tu-dortmun.de>
2021-06-09 11:22:15 +0200

    Update main_id_lstm.py

--- main_id_lstm.py
+++ main_id_lstm.py
@@ -301,7 +301,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                     'motionminers_flw': '/data/nnair/output/type1/momin/'}
     '''
     # GPU
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     GPU = 0
    
     # Labels position on the segmented window
