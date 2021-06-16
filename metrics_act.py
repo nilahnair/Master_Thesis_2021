8d256a19129eeb1686df5a3c9741551dd9f4e2eb
@nair <nilah.nair@tu-dortmun.de>
2021-06-16 18:32:57 +0200

    Update metrics_act.py

--- metrics_act.py
+++ metrics_act.py
@@ -27,9 +27,9 @@ class Metrics(object):
         #self,attr= network["att_rep"]
         self.attr = attributes
         if self.config['num_attributes'] == 4:
-            self.mid= self.attr[0:6,1:]
+            self.mid= self.attr[0:6,:]
         elif self.config['num_attributes'] == 10:
-            self.mid= self.attr[0:6,1:]
+            self.mid= self.attr[0:6,:]
             self.mid= np.concatenate((self.mid, self.attr[7:8,1:]), 0)
             
         print("printing mid")
