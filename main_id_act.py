b8d88947c3907d7721be0fc2d4157cafcd33f666
@nair <nilah.nair@tu-dortmun.de>
2021-06-17 10:58:02 +0200

    Update main_id_act.py

--- main_id_act.py
+++ main_id_act.py
@@ -21,7 +21,7 @@ from sacred import Experiment
 #from sacred.utils import apply_backspaces_and_linefeeds
 from sacred.observers import MongoObserver
 
-ex= Experiment('mocap no1 attr4 bceloss')
+ex= Experiment('mocap no2 attr4 bceloss')
 
 ex.observers.append(MongoObserver.create(url='curtiz',
                                          db_name='nnair_sacred',
@@ -76,9 +76,9 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
     #crossvalno0
     #num_tr_inputs = {'mocap': 147409, 'mbientlab': 126843, 'motionminers_flw': 93712}
     #crossvalno1
-    num_tr_inputs = {'mocap': 148510, 'mbientlab': 131501, 'motionminers_flw': 93712}
+    #num_tr_inputs = {'mocap': 148510, 'mbientlab': 131501, 'motionminers_flw': 93712}
     #crossvalno2
-    #num_tr_inputs = {'mocap': 147552, 'mbientlab': 129267, 'motionminers_flw': 93712}
+    num_tr_inputs = {'mocap': 147552, 'mbientlab': 129267, 'motionminers_flw': 93712}
     #crossvalno3
     #num_tr_inputs = {'mocap': 153266, 'mbientlab': 137512, 'motionminers_flw': 93712}
     #crossvalno4
@@ -222,7 +222,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
     elif output[output_idx] == 'attribute':
         labeltype = "attributes"
         #folder_base = "/data/nnair/output/attributes/all/mocap/"
-        folder_base = "/data/nnair/output/attributes/no1/mocap/output/"
+        folder_base = "/data/nnair/output/attributes/no2/mocap/output/"
         
     print("folderbase selected")
     print(folder_base)
@@ -296,7 +296,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                     'motionminers_flw': '/data/nnair/output/type1/momin/'}
     '''
     
-    dataset_root = {'mocap': '/data/nnair/output/attributes/no1/mocap/input/',
+    dataset_root = {'mocap': '/data/nnair/output/attributes/no2/mocap/input/',
                     'mbientlab': '/data/nnair/output/attributes/no7/imu/input/',
                     'motionminers_flw': '/data/nnair/output/type1/momin/'}
     
