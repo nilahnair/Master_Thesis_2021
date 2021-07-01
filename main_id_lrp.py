aa7396b1a149db00cc2874ee427be3354c262bdc
@nair <nilah.nair@tu-dortmun.de>
2021-07-01 16:43:02 +0200

    Update main_id_lrp.py

--- main_id_lrp.py
+++ main_id_lrp.py
@@ -451,11 +451,11 @@ def run(config, dataset, network, output, usageModus):
     
     network_obj = Network(config)
     print(network_obj)
-    print(network_obj[3].weight)
+    print(network_obj.layer[3].weight)
     network_obj.load_state_dict(torch.load('../Master_Thesis_2021/model/model_save_mocap.pt'))
     network_obj.eval()
     print(network_obj)
-    print(network_obj[3].weight)
+    print(network_obj.layer[3].weight)
     
     print("Done")
 
