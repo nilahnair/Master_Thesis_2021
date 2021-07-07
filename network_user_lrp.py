5021b9d7c68670d80c101c17dee76c9d9ea645c0
@nair <nilah.nair@tu-dortmun.de>
2021-07-07 20:12:29 +0200

    Update network_user_lrp.py

--- network_user_lrp.py
+++ network_user_lrp.py
@@ -1772,7 +1772,8 @@ class Network_User(object):
             test_v= torch.from_numpy(test_v)
             print(type(test_v))
             test_v= test_v.to(self.device, dtype=torch.float) 
-            test_l: torch.from_numpy(test_l)
+            test_l= torch.from_numpy(test_l)
+            print(type(test_l))
             test_l= test_l.to(self.device, dtype=torch.long)  
            
             
