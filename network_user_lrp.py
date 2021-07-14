a8b0ea03efaea4726caba65fe4805784722e903d
@nair <nilah.nair@tu-dortmun.de>
2021-07-14 11:02:14 +0200

    Update network_user_lrp.py

--- network_user_lrp.py
+++ network_user_lrp.py
@@ -1878,6 +1878,8 @@ class Network_User(object):
         print("length of layers")
         print(L)
         
+        print(network_obj.named_children())
+        
         convlayers1=layers[1:5]
         cl1=len(convlayers1)
         print(convlayers1)
