ee3e18563a3ff704d623d204f98668e34f85b128
@nair <nilah.nair@tu-dortmun.de>
2021-07-21 10:44:15 +0200

    Update main_op.py

--- main_op.py
+++ main_op.py
@@ -118,12 +118,12 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
     batch_size_train = {
         'cnn': {'locomotion': 100, 'gesture': 100, 'pamap2': 100},
         'lstm': {'locomotion': 100, 'gesture': 100, 'pamap2': 100},
-        'cnn_imu': {'locomotion': 100, 'gesture':100, 'pamap2': 200}}
+        'cnn_imu': {'locomotion': 50, 'gesture':100, 'pamap2': 200}}
 
     batch_size_val = {
         'cnn': {'locomotion': 100, 'gesture': 100, 'pamap2': 100},
         'lstm': {'locomotion': 100, 'gesture': 100, 'pamap2': 100},
-        'cnn_imu': {'locomotion': 100, 'gesture':100, 'pamap2': 200}}
+        'cnn_imu': {'locomotion': 50, 'gesture':100, 'pamap2': 200}}
     
      # Number of iterations for accumulating the gradients
     accumulation_steps = {'locomotion': 4, 'gesture': 4, 'pamap2': 4}
