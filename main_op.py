6d0c55b60b0dd75e8379c04186a2e60550cb46e4
@nair <nilah.nair@tu-dortmun.de>
2021-07-23 09:17:37 +0200

    Update main_op.py

--- main_op.py
+++ main_op.py
@@ -107,7 +107,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                         'cnn_imu': {'softmax': 10, 'attribute': 10}},
               'gesture': {'cnn': {'softmax': 10, 'attribute': 10},
                             'lstm': {'softmax': 10, 'attribute': 10},
-                            'cnn_imu': {'softmax': 5, 'attribute': 10}},
+                            'cnn_imu': {'softmax': 10, 'attribute': 10}},
               'pamap2': {'cnn': {'softmax': 10, 'attribute': 10},
                                    'lstm': {'softmax': 10, 'attribute': 10},
                                    'cnn_imu': {'softmax': 5, 'attribute': 10}}
