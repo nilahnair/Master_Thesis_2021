68ecfd2cf0295a153c0a4848afe0ea0eaee67600
@nair <nilah.nair@tu-dortmun.de>
2021-07-25 13:37:09 +0200

    Update main_op.py

--- main_op.py
+++ main_op.py
@@ -110,7 +110,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                             'cnn_imu': {'softmax':5, 'attribute': 10}},
               'pamap2': {'cnn': {'softmax': 10, 'attribute': 10},
                                    'lstm': {'softmax': 10, 'attribute': 10},
-                                   'cnn_imu': {'softmax': 10, 'attribute': 10}}
+                                   'cnn_imu': {'softmax': 50, 'attribute': 10}}
               } 
    #division_epochs = {'mocap': 2, 'mbientlab': 1, 'motionminers_flw': 1}
 
