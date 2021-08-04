7c137e799afb75efcddf28f8b32cd1464584d5dc
@nair <nilah.nair@tu-dortmun.de>
2021-08-04 09:01:34 +0200

    Update preprocessing_pamap.py

--- preprocessing_pamap.py
+++ preprocessing_pamap.py
@@ -491,23 +491,23 @@ def generate_data(target_filename):
                 
                 print("yl_train.shape")
                 print(yl_train.shape)
-                print("yl_.shape")
-                print(yl_train.shape)
-                print("yl_train.shape")
-                print(yl_train.shape)
+                print("yl_val.shape")
+                print(yl_val.shape)
+                print("yl_test.shape")
+                print(yl_test.shape)
         
                 
-                for i in range(yl_train.shape):
+                for i in range(len(yl_train)):
                     X_train = np.vstack(X_train, x[yl_train[i],:])
                     act_train = np.concatenate([act_train, y[yl_train[i],:]])
                     id_train = np.concatenate(id_train, counter)
                 
-                for i in range(yl_val.shape):
+                for i in range(len(yl_val)):
                     X_val = np.vstack(X_val, x[yl_val[i],:])
                     act_val = np.concatenate([act_val, y[yl_val[i],:]])
                     id_val = np.concatenate(id_val, counter)
                     
-                for i in range(yl_test.shape):
+                for i in range(len(yl_test)):
                     X_test = np.vstack(X_test, x[yl_test[i],:])
                     act_test = np.concatenate([act_test, y[yl_test[i],:]])
                     id_test = np.concatenate(id_test, counter)
