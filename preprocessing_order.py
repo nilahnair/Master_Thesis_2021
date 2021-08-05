a5ab076a8e6b03031629f7e0566d685faa70367a
@nair <nilah.nair@tu-dortmun.de>
2021-08-05 08:40:15 +0200

    Update preprocessing_order.py

--- preprocessing_order.py
+++ preprocessing_order.py
@@ -432,7 +432,7 @@ def generate_data(target_filename):
         print(yl_test.shape)
     
         print(yl_train[1])
-        print([data[int(yl_train[1])]].shape)
+        print(len([data[int(yl_train[1])]]))
         print(data[int(yl_train[1]),:,:])
     
         for i in range(len(yl_train)):
