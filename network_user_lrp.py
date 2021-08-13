e2b948022f696f57c9f8ec3212529d00b781dbf9
@nair <nilah.nair@tu-dortmun.de>
2021-08-13 12:41:22 +0200

    Update network_user_lrp.py

--- network_user_lrp.py
+++ network_user_lrp.py
@@ -1042,8 +1042,8 @@ class Network_User(object):
         model_dict = network_obj.state_dict()
         print("model dict with state dict loaded")
         print(model_dict)
-        print(model_dict.conv1_1.weight)
-
+        #print(model_dict.conv1_1.weight)
+        
         #print(network_obj)
         #print(network_obj.conv_LA_1_1.weight)
         if self.config["dataset"]=='mocap':
