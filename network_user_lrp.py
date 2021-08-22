076150c8ea553a92d941b017a1c6754d468b3db8
@nair <nilah.nair@tu-dortmun.de>
2021-08-22 13:40:02 +0200

    Update network_user_lrp.py

--- network_user_lrp.py
+++ network_user_lrp.py
@@ -1848,7 +1848,7 @@ class Network_User(object):
         
         for i in range(len(indxp1)):
             #947
-            if indxp1[i] == 950:
+            if indxp1[i] == 951:
                 lrp_test_indx.append(indxp1[i])
             elif indxp1[i] == 1106:
                 lrp_test_indx.append(indxp1[i])
@@ -1975,8 +1975,8 @@ class Network_User(object):
         tv = np.reshape(tv, newshape=(tv.shape[2], tv.shape[3]))
         print(tv.shape)
         
-        savetxt('relevance950.csv', p, delimiter=',')
-        savetxt('input950.csv', tv, delimiter=',')
+        savetxt('relevance951.csv', p, delimiter=',')
+        savetxt('input951.csv', tv, delimiter=',')
         
         '''
         A_LA[0] = (A_LA[0].data).requires_grad_(True)
