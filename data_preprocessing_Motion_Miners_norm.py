8a25bb52b5cded49fec6cb62a9f756179135e1b8
Nilah Ravi Nair <nilahnair.27@gmail.com>
2022-01-03 14:16:40 +0100

    lstm

--- data_preprocessing_Motion_Miners_norm.py
+++ data_preprocessing_Motion_Miners_norm.py
@@ -12,18 +12,20 @@ import csv
 import os
 import sys
 import datetime
-#import csv_reader
+import csv_reader
 from sliding_window import sliding_window
 import pickle
 
 FOLDER_PATH = "/vol/actrec/DFG_Project/2019/LARa_dataset/Motionminers/2019/flw_recordings_annotated/"
 
-NUM_CLASSES=7
+NUM_CLASSES=8
 SCENARIO = {'R01': 'L01', 'R02': 'L01', 'R03': 'L02', 'R04': 'L02', 'R05': 'L02', 'R06': 'L02', 'R07': 'L02',
             'R08': 'L02', 'R09': 'L02', 'R10': 'L02', 'R11': 'L02', 'R12': 'L02', 'R13': 'L02', 'R14': 'L02',
             'R15': 'L02', 'R16': 'L02', 'R17': 'L03', 'R18': 'L03', 'R19': 'L03', 'R20': 'L03', 'R21': 'L03',
             'R22': 'L03', 'R23': 'L03', 'R24': 'L03', 'R25': 'L03', 'R26': 'L03', 'R27': 'L03', 'R28': 'L03',
             'R29': 'L03', 'R30': 'L03'}
+labels_persons = {"S07": 0, "S08": 1, "S09": 2, "S10": 3, "S11": 4, "S12": 5, "S13": 6, "S14": 7}
+
 
 def opp_sliding_window(data_x, data_y, ws, ss, label_pos_end=True):
     data_x = sliding_window(data_x, (ws, data_x.shape[1]), (ss, 1))
@@ -55,23 +57,25 @@ def norm_motion_miners(data):
                   0.45731142,  78.58164965,  70.93038919,  76.34418105,   0.78003314,
                   0.32844988,   0.54919488,  26.68953896,  61.04472454,  62.9225945])
     '''
+    #all 
     
-    #type1
-    Mean values=np.array([-1789.84914,  1462.00361,  1.97059330e+03 -6.34871273e+00
-  9.67200065e+00 -1.93348175e+01  2.47001003e+03 -3.52440579e+03
- -2.15254172e+03  2.76902101e+02 -3.75078852e+03 -1.20081288e+03
- -4.70626042e+00  2.13210546e+00 -3.86406634e+00  6.81098169e+01
-  1.75337970e+03  6.02296986e+02  1.58659929e+03  1.17576727e+03
-  2.46183829e+03  7.51963186e+00 -1.28074702e+01  2.24440353e+01
- -2.35637780e+03 -2.32959081e+03 -2.36300845e+03])
-std values
-[1733.06158119 1978.77059693 1426.75854091  746.58999065  942.57832439
-  821.51338022 2381.00821892 2749.9204591  1996.81835605  698.10778208
-  605.42596235 1049.47539074  310.73720198  547.8695983   249.1646107
-  796.58605216  409.0788713  1209.5795624  1715.93353955 2000.82930488
- 1342.56436878  847.67775995  972.50805136  868.37830637 2326.84267029
- 2799.92887664 1947.71989224]
-    std_values = np.reshape(std_values, [1, 30])
+    mean_values=np.array([-1972.21093,  1319.18494,  2086.44939,  3.47782711,
+                          9.52718419, -15.4629411,  2839.43096, -2485.97015,
+                          -2057.10843,  106.437329, -3742.25358, -879.644817,
+                          -7.07292913,  7.37774481, -3.66716486, -184.263412,
+                          1747.75134,  528.392669,  1963.13183,  1187.77773,
+                          2204.39909,  4.49233848, -13.9301357,  19.7020469,
+                          -2115.97999, -2036.88632, -2350.28944])
+    mean_values = np.reshape(mean_values, [1, 27])
+    
+    std_values = np.array([1659.81581324, 1922.83004563, 1403.7565496,   803.60068613,  994.99489569,
+                           901.08895607, 2186.96453082, 2738.03382675, 1912.30333819,  812.34836189,
+                           594.45217882, 1313.82300176,  280.76318532,  580.97537463,  256.97295422,
+                           838.19728655,  423.39773434, 1117.36749732, 1744.18255968, 1942.39160641,
+                           1383.33653,    861.84780237, 1036.19127535,  923.98531812, 2604.48576624,
+                           2892.60854077, 1799.94353505])
+
+    std_values = np.reshape(std_values, [1, 27])
 
     mean_array = np.repeat(mean_values, data.shape[0], axis=0)
     std_array = np.repeat(std_values, data.shape[0], axis=0)
@@ -126,7 +130,7 @@ def reader_data(path):
             except KeyboardInterrupt:
                 print('\nYou cancelled the operation.')
 
-    if len(row) != 31:
+    if len(row) != 29:
         imu_data = {'IMU': IMU, 'time': time, 'data': data}
     else:
         imu_data = {'time': time, 'data': data}
@@ -161,7 +165,7 @@ def generate_data(ids, sliding_window_length, sliding_window_step, data_dir=None
     val_ids = ["R19", "R21"]
     test_ids = ["R22", "R23"]
     '''
-    
+    '''
     #type4-Avoiding persons 11,12,10
     
     persons = ["S07", "S08", "S09", "S13", "S14"]
@@ -169,6 +173,19 @@ def generate_data(ids, sliding_window_length, sliding_window_step, data_dir=None
     train_ids = ["R03", "R07", "R08", "R10", "R11", "R12", "R15", "R18", "R19", "R21", "R22"]
     val_ids = ["R23","R25", "R26"]
     test_ids = ["R27", "R28", "R29"]
+    '''
+    
+    labels_persons = {"S07": 0, "S08": 1, "S09": 2, "S10": 3, "S11": 4, "S12": 5, "S13": 6, "S14": 7}
+    
+    persons = [ "S07", "S08", "S09", "S10", "S11", "S12", "S13", "S14"]
+    
+    ID = {"S07": 0, "S08": 1, "S09": 2, "S10": 3, "S11": 4, "S12":5, "S13": 6, "S14": 7}
+    train_ids = ["R01", "R02", "R03", "R04", "R05", "R06", "R07", "R08", "R09", "R10", 
+                 "R13", "R14", "R16", "R17", "R18", "R19", "R20", "R21", "R22", "R23", 
+                 "R24", "R25", "R26", "R27", "R28", "R29", "R30"]
+    val_ids = ["R11","R12"]
+    test_ids = ["R15"]
+
     
     counter_seq = 0
     #hist_classes_all = np.zeros((NUM_CLASSES))
@@ -182,13 +199,16 @@ def generate_data(ids, sliding_window_length, sliding_window_step, data_dir=None
             elif usage_modus == 'val':
                 recordings = val_ids
             elif usage_modus == 'test':
-                recordings = test_ids
+                recordings = test_ids 
+        
         print("\nModus {} \n{}".format(usage_modus, recordings))
         for R in recordings:
                try:
                     S = SCENARIO[R]
                     file_name_data = "{}/{}_{}_{}.csv".format(P, S, P, R)
                     print("\n{}\n".format(file_name_data))
+                    file_name_label = "{}/{}_{}_{}_labels.csv".format(P, S, P, R)
+                    print("\n{}\n{}".format(file_name_data, file_name_label))
                     try:
                         data = reader_data(FOLDER_PATH + file_name_data)
                         print("\nFiles loaded in modus {}\n{}".format(usage_modus, file_name_data))
@@ -205,6 +225,7 @@ def generate_data(ids, sliding_window_length, sliding_window_step, data_dir=None
                             "2 In generating data, Error getting the data {}".format(FOLDER_PATH
                                                                                        + file_name_data))
                         continue
+                    
                     try:
                         data_x = norm_motion_miners(data_x)
                     except:
