64d43ebc3bde29371a4be40bf38210a22e805c53
Nilah Ravi Nair <nilahnair.27@gmail.com>
2022-06-01 04:46:48 +0200

    Update main_id_act.py

--- main_id_act.py
+++ main_id_act.py
@@ -21,7 +21,7 @@ from sacred import Experiment
 #from sacred.utils import apply_backspaces_and_linefeeds
 from sacred.observers import MongoObserver
 
-ex= Experiment('lara imu attr rep type 1 no5 euclidean 12')
+ex= Experiment('lara imu attr rep type 1 no6 euclidean 13')
 
 ex.observers.append(MongoObserver.create(url='curtiz',
                                          db_name='nnair_sacred',
@@ -70,7 +70,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
     num_attributes = {'mocap': 4, 'mbientlab': 4, 'motionminers_flw': 4}
     #num_attributes = {'mocap': 10, 'mbientlab': 10, 'motionminers_flw': 10, 'pamap':11}
     #all
-    num_tr_inputs = {'mocap': 172561, 'mbientlab': 138732, 'motionminers_flw': 139222, 'pamap':90280}
+    num_tr_inputs = {'mocap': 172561, 'mbientlab': 127658, 'motionminers_flw': 139222, 'pamap':90280}
     #num_tr_inputs = {'mocap': 172561, 'mbientlab': 147780, 'motionminers_flw': 161667}
     
     #attr without 6 and 7
@@ -240,7 +240,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
         #folder_base = "/data/nnair/output/attributes/no7/imu/output/"
         #folder_base = "/data/nnair/output/attributes/all/imu/"
         #folder_base = "/data/nnair/pamap/attr/output/exp1/"
-        folder_base = "/data/nnair/momin/attr/output/exp12/"
+        folder_base = "/data/nnair/momin/attr/output/exp13/"
         
     print("folderbase selected")
     print(folder_base)
@@ -300,7 +300,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
 
     # Paths are given according to the ones created in *preprocessing.py for the datasets
     dataset_root = {'mocap': '/data/nnair/output/attributes/no7/mocap/input/',
-                    'mbientlab': '/data/nnair/lara/attr/no5/imu/input/',
+                    'mbientlab': '/data/nnair/lara/attr/no6/imu/input/',
                     'motionminers_flw': '/data/nnair/momin/attr/no0/'}    
     '''
     dataset_root = {'mocap': '/data/nnair/output/activities/type4/mocap/',
@@ -325,7 +325,7 @@ def configuration(dataset_idx, network_idx, output_idx, usage_modus_idx=0, datas
                     'motionminers_flw': '/data/nnair/all/momin/'}
     '''
     # GPU
-    os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "4"
     GPU = 0
    
     # Labels position on the segmented window
